4ccdb28a408b0f8c5e8ef7417b630491deafc6a0
Baghiut Andreea <abaghiut@gmail.com>
2023-10-21 21:41:59 +0300

    Add project files.

---
+++ Baghiut_Andreea_Lab2/Models/Book.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using NuGet.Packaging.Signing;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Baghiut_Andreea_Lab2.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Book Title")]
+        public string Title { get; set; }
+
+        [Column(TypeName = "decimal(6, 2)")]
+
+        public decimal Price { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime PublishingDate { get; set; }
+        public int? PublisherID { get; set; }
+        public Publisher? Publisher { get; set; }
+
+        public int? AuthorId { get; set; } //cheie straina
+
+        public Author? Author {  get; set; } //proprietate de navigare
+    }
+}
